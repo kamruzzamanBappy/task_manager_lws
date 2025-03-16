5c804d96bbf0ee9b68a04db1ca9d6e556387c545
kamruzzamanBappy <kamruzzamanbappy13@gmail.com>
2025-03-16 20:05:47 +0600

    d3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import HeroSection from "./HeroSection";
+import TaskBoard from "./task/TaskBoard";
 
 export default function App() {
   return (
     <>
       <Header />
-      <HeroSection />
+      <div className="flex flex-col justify-center items-center">
+        <HeroSection />
+        <TaskBoard />
+      </div>
+
       <Footer />
     </>
   );
