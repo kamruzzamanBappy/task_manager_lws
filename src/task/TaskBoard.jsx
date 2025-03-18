19054241b34c0712f31c5e4bc7530e096b784a45
kamruzzamanBappy <kamruzzamanbappy13@gmail.com>
2025-03-18 15:14:11 +0600

    d4

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import React from "react";
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
@@ -16,15 +17,25 @@ const TaskBoard = () => {
   };
 
   const [tasks, setTasks] = useState([defaultTask]);
+
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  function handleAddTask(newTask) {
+    console.log("ADD A TASK", newTask);
+    setTasks([...tasks, newTask]);
+    setShowAddModal(false);
+  }
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskModal onSave={handleAddTask} />}
+
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions />
+          <TaskActions onAddClick={() => setShowAddModal(true)} />
           <TaskList tasks={tasks} />
         </div>
       </div>
